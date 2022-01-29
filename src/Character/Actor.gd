ec4cf77ac3c617b0d72388649003197ea93c7b2b
Baptiste Domange <baptiste.domange@gmail.com>
2022-01-29 15:42:17 +0100

    paralax background

--- src/Character/Actor.gd
+++ src/Character/Actor.gd
@@ -4,7 +4,7 @@ class_name Actor
 
 const FLOOR_NORMAL: = Vector2.UP
 
-export var speed: = Vector2(800.0, 1000)
+export var speed: = Vector2(400.0, 650)
 export var gravity := 3000.0
 export var life_point := 3
 var _velocity: = Vector2.ZERO
